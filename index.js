6cadedd23f3f28c192447881a849a416a76faf8a
unknown <ahsnova557@gmail.com>
2024-09-22 15:00:18 +0400

    task added

---
+++ index.js
@@ -0,0 +1,71 @@
+telebeler = [
+    {
+        ad : "Aysen",
+        soyad : "Hesenova",
+        kursIshiQiymet : 10,
+        isPassed : true
+
+
+    },
+    {
+        ad : "Emine",
+        soyad : "Abayeva",
+        kursIshiQiymet : 10,
+        isPassed : true
+    },
+    {
+        ad : "Leyla",
+        soyad : "Huseynli",
+        kursIshiQiymet : 3,
+        isPassed : false
+    },
+    {
+        ad : "Sebine",
+        soyad : "Qurbanova",
+        kursIshiQiymet : 8,
+        isPassed : true,
+    },
+    {
+        ad : "Musa",
+        soyad : "Hesenov",
+        kursIshiQiymet : 2,
+        isPassed : false
+    },
+    {
+        ad : "Seid",
+        soyad : "Manafzade",
+        kursIshiQiymet : 10,
+        isPassed : true,
+    },
+    {
+        ad : "Kerim",
+        soyad : "Eliyev",
+        kursIshiQiymet : 4,
+        isPassed : false,
+    },
+    {
+        ad : "Fuad",
+        soyad : "Muradov",
+        kursIshiQiymet : 7,
+        isPassed : true,
+    },
+    {
+        ad : "Azer",
+        soyad : "Babayev",
+        kursIshiQiymet : 2,
+        isPassed : false,
+    },
+    {
+        ad : "Elcan",
+        soyad : "Huseynov",
+        kursIshiQiymet : 9,
+        isPassed : true,
+    }
+
+    
+]
+
+for (var i = 0; i < telebeler.length; i++) {
+    console.log(telebeler[i])
+    
+}
